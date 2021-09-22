3463b7893017c08c54c365427914df6b6845a581
Bruno Boettcher <bboett@nohkumado.eu>
2021-09-22 15:37:46 +0200

    added start of unit testing

---
+++ test/kpl_test.dart
@@ -0,0 +1,41 @@
+import 'package:test/test.dart';
+import 'package:nohfibu/kto_plan.dart';
+import 'package:nohfibu/journal.dart';
+
+void main() {
+  late KontoPlan kpl;
+  late Journal jrl;
+  setUp(()
+      {
+	kpl = KontoPlan();
+	  jrl = Journal( kpl);
+      });
+  group('Konto', () {
+    // 
+    //Konto set({number,name= "kein Name", plan, valuta, cur, budget})
+    //Konto get(String ktoName)
+    //String toString({String indent: ""})
+    //printname()
+    //void asList(List<List> asList)
+    test('Konto ', () {
+      var kto = Konto(number : "1",name: "1001", plan: kpl, valuta:10000.99, cur:"EUR", budget:999.12);
+      expect(kto.name, equals("1001"));
+    });
+
+    //test('.trim() removes surrounding whitespace', () {
+    //  var string = '  foo ';
+    //  expect(string.trim(), equals('foo'));
+    //});
+  });
+
+  //group('int', ()
+  //{
+  //  test('.remainder() returns the remainder of division', () {
+  //    expect(11.remainder(3), equals(2));
+  //  });
+
+  //  test('.toRadixString() returns a hex string', () {
+  //    expect(11.toRadixString(16), equals('b'));
+  //  });
+  //});
+}
