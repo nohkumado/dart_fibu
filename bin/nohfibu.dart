852c151a3139363a9c7872f3d108fe61ef03b0c3
Bruno Boettcher <nohkumado@gmail.com>
2021-10-05 20:53:28 +0200

    parses date in fibu fastop

--- bin/nohfibu.dart
+++ bin/nohfibu.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:intl/intl.dart';
 
 import 'package:nohfibu/settings.dart';
 import 'package:nohfibu/csv_handler.dart';
@@ -30,6 +31,82 @@ class Fibu {
     result += book.kpl.analysis();
     return result;
   }
+  ///Execute a preprared statement
+  void opExe(String key)
+  {
+    print("we need to call on fast op ${key}");
+    Operation? actOp = book.ops[key];
+    if(actOp == null) print("Fast op '${key}' unknown, plese check the name");
+    else
+    {
+      print("Found fast op '${actOp}' ");
+      actOp.prepare();
+      actOp.preparedLines.forEach((line) 
+	  {
+	    print("to fill $line"); 
+	    final DateFormat formatter = DateFormat('dd-MM-yyyy');
+	    final String formatted = formatter.format(line.datum);
+	    print("Date[$formatted]");
+	    bool invalid = true;
+	    while(invalid)
+	    {
+	      String? answer = stdin.readLineSync();
+	      answer ??= "";
+	      if( answer.isNotEmpty)
+	      {
+		print("answered: '$answer'");
+		try
+		{
+		  //DateTime point = DateTime.parse(answer);
+        DateFormat format = DateFormat("dd-MM-yyyy");
+        //print("extracted so far +$datum+ -$kminus- -$kplus- -$desc- ,=$w=, #$valuta#");
+        var point = format.parse(answer);
+		  line.datum = point;
+		  invalid =false;
+		}
+		catch(e) 
+		{
+		  //print("couldn*t undestand the date....");
+		  try
+		  {
+		    //DateTime point = DateTime.parse(answer);
+		    DateFormat format = DateFormat("dd-MM-yy");
+		    //print("extracted so far +$datum+ -$kminus- -$kplus- -$desc- ,=$w=, #$valuta#");
+		    var point = format.parse(answer);
+		    line.datum = point;
+		    invalid =false;
+		  }
+		  catch(e) {
+		  try
+		  {
+		    //DateTime point = DateTime.parse(answer);
+        DateFormat format = DateFormat("dd.MM.yyyy");
+		    //print("extracted so far +$datum+ -$kminus- -$kplus- -$desc- ,=$w=, #$valuta#");
+		    var point = format.parse(answer);
+		    line.datum = point;
+		    invalid =false;
+		  }
+		  catch(e) {
+		  try
+		  {
+		    //DateTime point = DateTime.parse(answer);
+        DateFormat format = DateFormat("dd.MM.yy");
+		    //print("extracted so far +$datum+ -$kminus- -$kplus- -$desc- ,=$w=, #$valuta#");
+		    var point = format.parse(answer);
+		    line.datum = point;
+		    invalid =false;
+		  }
+		  catch(e) {print("couldn't understand the date....");}
+		  }
+		  }
+		}
+	      }
+	      else invalid =false;
+	    }
+	  }
+      );
+    }
+  }
 }
 
 main(List<String> arguments) //async
@@ -38,11 +115,11 @@ main(List<String> arguments) //async
   Settings settings = Settings();
   settings..parser.addFlag('run',
       abbr: 'r', defaultsTo: false, help: "run the accounting process")
-..parser.addFlag('list',
-       defaultsTo: false, help: "list the available fast ops")
+      ..parser.addFlag('list',
+	  defaultsTo: false, help: "list the available fast ops")
 
-  ..parser.addOption('fastop',
-      abbr: 'f',  help: "call fast operation <name>");
+      ..parser.addOption('fastop',
+	  abbr: 'f',  help: "call fast operation <name>");
   settings.init(arguments);
   Fibu fibu = Fibu();
 
@@ -64,30 +141,19 @@ main(List<String> arguments) //async
     if (settings["run"]) {
       String result = fibu.execute();
       fname = (settings["output"].isNotEmpty)
-          ? settings["output"]
-          : basename + ".lst";
+	  ? settings["output"]
+	  : basename + ".lst";
       //print ("retrieved\n$result");
       File(fname).writeAsString(result).then((file) {
 	print("write seems successful, please check $fname");
       });
     } else if (settings["list"]) {
       print("Available fast operations:");
-    fibu.book.ops.forEach((key, val)=>print("$key"));
+      fibu.book.ops.forEach((key, val)=>print("$key"));
       print("End of List");
     }
-   else if (settings["fastop"].isNotEmpty) {
-      print("we need to call on fast op ${settings['fastop']}");
-    Operation? actOp = fibu.book.ops[settings['fastop']];
-    if(actOp == null)
-    {
-      print("Fast op '${settings['fastop']}' unknown, plese check the name");
-    }
-    else
-    {
-      print("Found fast op '${actOp}' ");
-      actOp.prepare();
-      actOp.preparedLines.forEach((line) => print("to fill $line"));
-    }
+    else if (settings["fastop"].isNotEmpty) {
+      fibu.opExe(settings["fastop"]);
     } else
       print("book so far: ${fibu.book}");
   } else
